8d1a8de50a1339c37b4780d8c5f0ca4dc49bd6d5
sangram-2003 <cste.sangram2003@gmail.com>
2024-11-18 12:53:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,7 @@ server.use("/contacts", contactRouter);
 server.use("/dsa", dsaRouter);
 
 // Start the server
-server.listen(8080, () => {
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
   console.log("Server started on port 8080");
 });
